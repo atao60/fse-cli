c565e13ed716e054206b5d14bbf8eb77c2f9708d
Pierre Raoul <p.m.raoul@gmail.com>
2020-01-08 12:33:06 +0100

    IT: check create & del dir

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, mkdtempSync, mkdirSync, rmdirSync, statSync } from 'fs';
 import { describe, it } from 'mocha';
 import { join } from 'path';
 import { env } from 'process';
@@ -7,7 +7,7 @@ import { env } from 'process';
 import { ENTER, execute as run } from './cmd';
 
 const LIB_DIR = join(__dirname, env.APP_CODE_PATH || '../../dist');
-const TARGET_DIR = join(__dirname, env.PROJECT_TARGET_PATH || '../.tmp-dir');
+const TMP_DIR = join(__dirname, env.PROJECT_TARGET_PATH || '../.tmp-dir');
 
 describe('The fse CLI project', () => {
 
@@ -63,5 +63,84 @@ describe('The fse CLI project', () => {
                 done(error);
             });
     });
+
+    // first check if the jog is done
+    it("Remove a directory", function (done) { // don't pass 'done' as argument! with async/await
+        let newDirPath: string = null;
+        try {
+            mkdirSync(TMP_DIR, { recursive: true });
+            newDirPath = mkdtempSync(join(TMP_DIR, 'fse-cli-test-'));
+        } catch (e) {
+            throw new Error(`Test 'Remove a directory', unable to create a temporaray directory '${newDirPath}': ${e}`);
+        }
+
+        const dirToBeRemoved = newDirPath;
+        const script = `${LIB_DIR}`; // ie index.js
+        const args = { app: ['remove', dirToBeRemoved] };
+        const userInputs = [];
+        const options = {
+            // env: { DEBUG: true },  // false by default
+            // timeout: 200,          // 100 ms by default
+            // maxTimeout: 0          // 10 s by default; if "0" then no timeout
+        };
+        run(
+            script,
+            args,
+            userInputs,
+            options
+        )
+            .then(() => {
+                expect(existsSync(dirToBeRemoved)).to.be.false;
+                done();
+            })
+            .catch(error => {
+                done(error);
+            })
+            .finally(() => {
+                if (existsSync(dirToBeRemoved))
+                    rmdirSync(dirToBeRemoved, { recursive: true });
+            });
+    });
+
+    // first check if the jog is done
+    it("Create a directory", function (done) { // don't pass 'done' as argument! with async/await
+        let newDirPath: string = null;
+        try {
+            mkdirSync(TMP_DIR, { recursive: true });
+            newDirPath = mkdtempSync(join(TMP_DIR, 'fse-cli-test-'));
+        } catch (e) {
+            throw new Error(`Test 'Create a directory', before starting unable to create a temporaray directory '${newDirPath}': ${e}`);
+        }
+
+        const baseDir = newDirPath;
+        const dirToBeCreated = join(baseDir, 'theNewDir');
+        const script = `${LIB_DIR}`; // ie index.js
+        const args = { app: ['mkdirp', dirToBeCreated] };
+        const userInputs = [];
+        const options = {
+            // env: { DEBUG: true },  // false by default
+            // timeout: 200,          // 100 ms by default
+            // maxTimeout: 0          // 10 s by default; if "0" then no timeout
+        };
+        run(
+            script,
+            args,
+            userInputs,
+            options
+        )
+            .then(() => {
+                expect(existsSync(dirToBeCreated)).to.be.true;
+                expect(statSync(dirToBeCreated).isDirectory()).to.be.true;
+                done();
+            })
+            .catch(error => {
+                done(error);
+            })
+            .finally(() => {
+                rmdirSync(dirToBeCreated, { recursive: true });  // to avoid ENOTEMPTY with next rmdirSync, even with recursive: true
+                rmdirSync(baseDir, { recursive: true });
+            });
+    });
+
 
 });
