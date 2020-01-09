f30acf6bd06fa4b96c22465e591a9a41cbd99ea3
Pierre Raoul <p.m.raoul@gmail.com>
2020-01-09 09:18:31 +0100

    FIX: add forgotten emptyDir

--- src/config.ts
+++ src/config.ts
@@ -4,15 +4,21 @@ import { prompt } from 'inquirer';
 import { basename, join } from 'path';
 import { exit } from 'process';
 
-import { copyDef, ensureDirDef, removeDef } from './tasks';
+import { copyDef, emptyDirDef, ensureDirDef, removeDef } from './tasks';
 
+// TODO: load the list of files under ./tasks but index.ts then lazy load the required ones
+//       create handle task file for the aliases
+//       create at least one test per jobDef to check than even the bin list in package.json is up-to-date
+//       do it even for the aliases
+//       create a npm script in package.json to update the bin list in itself?
 const jobDefs = {
     copy: copyDef,
     remove: removeDef,
     rimraf: { alias: 'remove' },
     ensureDir: ensureDirDef,
     mkdirs: { alias: 'ensureDir' },
-    mkdirp: { alias: 'ensureDir' }
+    mkdirp: { alias: 'ensureDir' },
+    emptyDir: emptyDirDef
 }
 
 // 'cli' must not be used as task name
