91c57e6f7b796d062f3b4be65f2717a3a233573f
atao <p.m.raoul@gmail.com>
2020-08-31 11:58:20 +0200

    fix(scripts): remove any deps of rmdir script

---
+++ scripts/rmdir.js
@@ -0,0 +1,34 @@
+/**
+ *   Required under Windows, as `rimraf` can't delete itself under folder `./node_modules`.
+ *   
+ *   As this script is supposed to remove `node_modules`, don't use ts-node or any
+ *   dependencies (not even graceful-fs), as it'd block the script, even under Linux.
+ */
+import { existsSync, lstatSync, readdirSync, rmdirSync, unlinkSync } from 'fs';
+import { join } from 'path';
+import { argv, env, exit } from 'process';
+
+const pathList = [...argv.slice(2)] || [env.PATHS] || ['node_modules/'];
+
+if (!pathList || pathList.length < 1) {
+    console.log("File/folder list is empty or not provided: nothing's done.");
+    exit(1);
+}
+
+function deepDelete(paths) {
+    paths.forEach(p => {
+        if (!existsSync(p)) {
+            console.log(`${p} doesn't exist: skip removing this file/folder.`);
+        }
+        else if (!lstatSync(p).isDirectory()) {
+            unlinkSync(p);
+        }
+        else {
+            const subPaths = readdirSync(p).map(subp => join(p, subp));
+            deepDelete(subPaths);
+            rmdirSync(p);
+        }
+    });
+}
+
+deepDelete(pathList);
