aec23eec3bec8fa7ad33ed6a951a63383315ea05
atao <p.m.raoul@gmail.com>
2020-11-11 08:48:54 +0100

    refactor: remove babel-register.js and update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,40 +1,52 @@
+const targets = {
+    node: "10.15.3" // See also engines.node in package.json
+    // With @atao/fse-cli, indeed no '.browserslistrc'
+};
+
+const plugins = [
+    "@babel/transform-typescript",
+    // must be set before @babel/proposal-decorators
+    "babel-plugin-transform-typescript-metadata",
+    [   // must be set before @babel/proposal-class-properties
+        "@babel/proposal-decorators",
+        {
+            legacy: true // 'legacy === true' forces using Typescript decorator specs, not EcmaScript ones
+        }
+    ],
+    [
+        "@babel/proposal-class-properties",
+        {
+            loose: true // 'loose === true' is required by proposal-decorators' legacy
+        }
+    ],
+    "@babel/plugin-transform-runtime", // provided defs such as _interopRequireDefault
+    "babel-plugin-node-source-map-support"
+];
+
+const presets = [
+    "@babel/typescript",
+    [
+        "@babel/env",
+        {
+            // "debug": true,
+            // 'usage': includes polyfills given `.browserslistrc` and your source code (Babel analyses it - might not always 
+            //  perfectly work depending on your app and its dependencies) instead of including everything from core-js
+            useBuiltIns: "usage",
+            corejs: {
+                version: 3,
+                proposals: true // will enable polyfilling of every proposal supported by core-js
+            },
+            // Now Babel defaults 'modules' to 'auto', not any more to 'commonjs' (which won’t tree-shake.)
+            // So no more need to setup 'modules' to 'false':
+            // modules: false, 
+            targets
+        }
+    ]
+];
+
 module.exports = (api) => {
     // Cache configuration is a required option
-    api.cache(false);
-
-    const plugins = [
-        "@babel/transform-typescript",
-        // must be set before @babel/proposal-decorators
-        "babel-plugin-transform-typescript-metadata",
-        [   // must be set before @babel/proposal-class-properties
-            "@babel/proposal-decorators",
-            {
-                "legacy": true // 'legacy === true' forces using Typescript decorator specs, not EcmaScript ones
-            }
-        ],
-        [
-            "@babel/proposal-class-properties",
-            {
-                "loose": true // 'loose === true' is required by proposal-decorators' legacy
-            }
-        ],
-        "babel-plugin-node-source-map-support"
-    ];
-
-    const presets = [
-        "@babel/typescript",
-        [
-            "@babel/env",
-            {
-                "targets": {
-                    "node": "10.15.3" // Node version should be at most equal to engine.nodes in package.json
-                },
-                // "debug": true,
-                "useBuiltIns": "usage",
-                "corejs": 3
-            }
-        ]
-    ];
+    api.cache(true); // Same as api.cache.forever(), ie permacache the computed config and never call the function again
 
     return { presets, plugins };
 };
